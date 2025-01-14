03a99bc9a2642ab650dd38ff2ffb9a8a0447a751
Arziki Pratama <arzi1605@gmail.com>
2025-01-14 23:36:22 +0700

    feat: add anchor trigger for animations, remove unneeded functions

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,13 +7,26 @@ import Image from "next/image";
 
 export const Footer = () => {
   return (
-    <Box sx={{ width: "100vw", backgroundColor: "rgba(36,36,36,1)", height: "95vh" }}>
-      <Typography color="#FFF" fontWeight={800} variant="h2" pt={"8vh"} pb={"8vh"} textAlign={"center"} width={"39vw"} mx={"auto"} data-aos="fade-up" data-aos-easing="ease-in-out">
+    <Box sx={{ width: "100vw", backgroundColor: "rgba(36,36,36,1)", height: "95vh" }} id="trigger-f">
+      <Typography
+        color="#FFF"
+        fontWeight={800}
+        variant="h2"
+        pt={"8vh"}
+        pb={"8vh"}
+        textAlign={"center"}
+        width={"39vw"}
+        mx={"auto"}
+        data-aos="fade-up"
+        data-aos-easing="ease-in-out"
+        data-aos-anchor="#trigger-f"
+        data-aos-anchor-placement="top-center"
+      >
         Paint your vision, shape your home.
       </Typography>
       <Stack spacing={5} pt={"10.5vh"}>
         <Stack direction={"row"} justifyContent={"space-between"} px={"3.5vw"}>
-          <Stack direction={"row"} spacing={3} pr={"5vw"} data-aos="fade-right" data-aos-easing="ease-in-out">
+          <Stack direction={"row"} spacing={3} pr={"5vw"} data-aos="fade-right" data-aos-easing="ease-in-out" data-aos-anchor="#trigger-f" data-aos-anchor-placement="top-center">
             <Link underline="hover" color="#FFF">
               Home
             </Link>
@@ -30,7 +43,7 @@ export const Footer = () => {
               Contact Us
             </Link>
           </Stack>
-          <Stack direction={"row"} justifyContent={"center"} spacing={4} data-aos="fade-left" data-aos-easing="ease-in-out">
+          <Stack direction={"row"} justifyContent={"center"} spacing={4} data-aos="fade-left" data-aos-easing="ease-in-out" data-aos-anchor="#trigger-f" data-aos-anchor-placement="top-center">
             <IconButton sx={{ p: 0 }}>
               <Image src={linkedin} width={25} height={25} alt="linkedin" />
             </IconButton>
@@ -45,7 +58,7 @@ export const Footer = () => {
             </IconButton>
           </Stack>
         </Stack>
-        <Typography color="#FFF" fontWeight={800} fontSize={"19.931vw"} lineHeight={"18.652vh"} pl={"2vw"} data-aos="fade-up" data-aos-easing="ease-in-out">
+        <Typography color="#FFF" fontWeight={800} fontSize={"19.931vw"} lineHeight={"18.652vh"} pl={"2vw"} data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-anchor="#trigger-f" data-aos-anchor-placement="top-center">
           DAMDEX
         </Typography>
       </Stack>
