10e1194d3acfb9d8f81b6656e551de2c7ab265ff
Arziki Pratama <arzi1605@gmail.com>
2025-06-24 20:57:33 +0700

    feat: added and changed image to product details page

--- src/app/product/warna/page.tsx
+++ src/app/product/warna/page.tsx
@@ -4,9 +4,10 @@ import React, { useEffect, useState } from "react";
 import { SixthSection } from "@/components/sixthSection";
 import { AppLayout } from "@/components/appLayout";
 import { Stack, Box, Typography, IconButton, useTheme, useMediaQuery } from "@mui/material";
-import img1 from "@/../public/productheader.png";
-import img2 from "@/../public/fungsi4.png";
-import sizeImg from "@/../public/productsize.png";
+import img from "@/../public/headerwarna.png";
+import logowarna from "@/../public/Damdex Warna Logo-01.png";
+import produk from "@/../public/produkwarna.png";
+import colors from "@/../public/pilihanwarna.png";
 import Image, { StaticImageData } from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
@@ -43,232 +44,259 @@ const Page = () => {
 
   return (
     <AppLayout>
-      <Stack
-        sx={{ alignItems: { md: "center" }, pt: { xl: 6, lg: 4, xs: 6 } }}
-        direction={"row"}
-      >
+      <Box sx={{ pt: { xl: "100px", lg: "80px", md: "80px", sm: "75px", xs: "75px" } }}>
         <Box
+          position={"relative"}
+          mx={"auto"}
           sx={{
-            width: { xs: "100vw", md: "90vw", lg: "90vw" },
-            height: { xs: "150px", sm: "190px", md: "220px", lg: "40vh" },
-            position: "relative",
-            backgroundColor: "rgba(0,0,0,1)",
+            width: { xs: "50vw", md: "30vw", lg: "25vw" },
+            height: { xs: "100px", sm: "125px", md: "125px", xl: "200px" },
           }}
         >
           <Image
-            alt="image1"
-            src={img1}
+            src={logowarna}
             priority
+            alt="logo"
             fill
           />
         </Box>
-        <Box
-          sx={{
-            width: { xs: "100vw", md: "90vw", lg: "90vw" },
-            height: { xs: "150px", sm: "190px", md: "220px", lg: "40vh" },
-            position: "relative",
-            backgroundColor: "rgba(0,0,0,1)",
-          }}
-        >
-          <Image
-            alt="image2"
-            src={img2}
-            priority
-            fill
-          />
-        </Box>
-        <div>
-          <Box
-            sx={{
-              height: "11vh",
-              width: "58vw",
-              position: "absolute",
-              left: "23vw",
-              zIndex: 5,
-              top: { xl: "20vh", lg: "18vh", md: "120px", sm: "12vh", xs: "100px" },
-            }}
-          >
-            <Stack alignItems={"center"}>
-              <Typography
-                color="#FFF"
-                width={"80vw"}
-                textAlign={"center"}
-                sx={{ typography: { xl: "h2", md: "h3" } }}
-              >
-                ONE PRODUCT
-              </Typography>
-              <Typography
-                color="#FFF"
-                width={"80vw"}
-                textAlign={"center"}
-                sx={{ typography: { xl: "h2", md: "h3" } }}
-              >
-                ALL SOLUTIONS
-              </Typography>
-            </Stack>
-          </Box>
-          <Box
-            sx={{
-              height: { xl: "4vh", lg: "4vh", md: "3.5vh", sm: "1.5vh", xs: "2.5vh" },
-              width: "45vw",
-              backgroundColor: "#37B878",
-              position: "absolute",
-              top: { xl: "23.25vh", lg: "22vh", md: "145px", sm: "13vh", xs: "110px" },
-              left: { xs: "29.3vw" },
-            }}
-          />
-        </div>
-      </Stack>
-      <Stack
-        p={5}
-        fontSize={{ xs: "20px", lg: "26px" }}
-      >
-        <div
-          className="flex flex-col !space-y-10"
-          dangerouslySetInnerHTML={{
-            __html: language === "id" ? (data?.data.descriptionIDN as string) : (data?.data.descriptionENG as string),
-          }}
-        />
-      </Stack>
-
-      {/*Carousel */}
-      {data && data.data.productAdvantage && (
-        <Box p={{ xs: 5, lg: 10 }}>
-          <Typography
-            gutterBottom
-            fontWeight={800}
-            sx={{ fontWeight: "bold", fontSize: { xl: "34px", md: "24px" } }}
-          >
-            Additive with Multi Purpose
-          </Typography>
-
-          <Box
-            sx={{ overflow: "hidden", position: "relative" }}
-            ref={emblaRef}
-          >
-            <Box sx={{ display: "flex" }}>
-              {data?.data.productAdvantage.map((slide, index) => (
-                <Box
-                  key={index}
-                  sx={{
-                    minWidth: { xl: "20%", lg: "25%", md: "30%", sm: "40%", xs: "60%" },
-                    padding: 2,
-                    borderRadius: "25px",
-                    position: "relative",
-                    height: { lg: "500px", sm: "400px", xs: "300px" },
-                    mx: 2,
-                    backgroundColor: "rgba(0,0,0,1)",
-                    cursor: "pointer",
-                  }}
-                >
-                  <div
-                    onClick={() =>
-                      setAdvantage({
-                        title: language === "eng" ? slide.titleENG : slide.titleIDN,
-                        description: language === "eng" ? slide.descriptionENG : slide.descriptionIDN,
-                        image: slide.heroImageUrl,
-                      })
-                    }
-                  >
-                    <Image
-                      src={slide.heroImageUrl}
-                      alt={language === "eng" ? slide.titleENG : slide.titleIDN}
-                      layout="fill"
-                      objectFit="cover"
-                      style={{ borderRadius: "25px" }}
-                      priority
-                      unoptimized
-                    />
-                    <Box
-                      sx={{
-                        position: "absolute",
-                        top: 0,
-                        width: "90%",
-                        color: "white",
-                        pt: 2,
-                        px: 1,
-                      }}
-                    >
-                      <Typography
-                        alignSelf={"start"}
-                        fontSize={{ xs: "13px", sm: "15px", md: "16px", lg: "16px", xl: "22px" }}
-                      >
-                        {language === "eng" ? slide.titleENG : slide.titleIDN}
-                      </Typography>
-                    </Box>
-                  </div>
-                </Box>
-              ))}
-            </Box>
-          </Box>
-          {/*Carousel */}
-
-          <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
-            <IconButton
-              onClick={scrollPrev}
-              sx={{ mx: 1 }}
-            >
-              <ArrowBackIosIcon sx={{ color: "#000" }} />
-            </IconButton>
-            <IconButton
-              onClick={scrollNext}
-              sx={{ mx: 1 }}
-            >
-              <ArrowForwardIosIcon sx={{ color: "#000" }} />
-            </IconButton>
-          </Box>
-        </Box>
-      )}
-
-      <AdvantageDialog
-        description={advantage?.description}
-        img={advantage?.image}
-        open={Boolean(advantage)}
-        onClose={() => setAdvantage(undefined)}
-        title={advantage?.title}
-      />
-
-      <Box sx={{ width: { lg: "100vw" }, height: { lg: "65vh", xs: "30vh" }, position: "relative" }}>
-        <Image
-          alt="image1"
-          src={sizeImg}
-          fill
-          style={{ objectFit: desktop ? "cover" : "contain" }}
-          priority
-        />
-      </Box>
-      <SixthSection noBackground />
-      <Box
-        sx={{
-          width: { xs: "100vw" },
-          height: "30vh",
-          px: 2,
-          background: "linear-gradient(89.7deg, #B94BBC -18.1%, #137C3B 115.45%)",
-          backgroundSize: "cover",
-        }}
-      >
         <Stack
-          alignItems={"center"}
-          justifyItems={"center"}
+          sx={{ alignItems: { md: "center" }, pt: { xl: 6, lg: 4, sm: 2, xs: 2 } }}
           direction={"row"}
-          spacing={4}
-          sx={{ pt: { xs: "12vh" } }}
         >
-          <Typography
-            variant="h2"
-            fontWeight={800}
-            color="rgba(255, 255, 255, 1)"
-            textAlign={"center"}
-            width={"65vw"}
-            sx={{ fontSize: { xl: "45px", md: "30px", xs: "15px" } }}
+          <Box
+            sx={{
+              width: "100vw",
+              height: { xs: "150px", sm: "190px", md: "220px", lg: "250px", xl: "40vh" },
+              position: "relative",
+              backgroundColor: "rgba(0,0,0,1)",
+            }}
           >
-            Vibrant hues endless possibilities
-          </Typography>
-          <FilledButton
-            label="View Product"
-            onClick={() => router.push("/product/multifungsi")}
+            <Image
+              alt="image1"
+              src={img}
+              priority
+              fill
+            />
+          </Box>
+          <div>
+            <Box
+              sx={{
+                height: "11vh",
+                width: "58vw",
+                position: "absolute",
+                left: "23vw",
+                zIndex: 5,
+                top: { xl: "450px", lg: "300px", md: "275px", sm: "275px", xs: "235px" },
+              }}
+            >
+              <Stack alignItems={"center"}>
+                <Typography
+                  color="#FFF"
+                  width={"80vw"}
+                  fontWeight={800}
+                  textAlign={"center"}
+                  sx={{ typography: { xl: "h2", md: "h3", sm: "h4", xs: "h5" } }}
+                >
+                  PIONEER OF
+                </Typography>
+                <Typography
+                  color="#FFF"
+                  width={"80vw"}
+                  textAlign={"center"}
+                  fontWeight={800}
+                  sx={{ typography: { xl: "h2", lg: "h3", md: "h4", sm: "h5", xs: "h6" } }}
+                >
+                  WATERPROOFING INNOVATION
+                </Typography>
+              </Stack>
+            </Box>
+            <Box
+              sx={{
+                height: { xl: "4vh", lg: "4vh", md: "2.5vh", sm: "1.5vh", xs: "2.5vh" },
+                width: "45vw",
+                backgroundColor: "#37B878",
+                position: "absolute",
+                top: { xl: "485px", lg: "335px", md: "305px", sm: "295px", xs: "250px" },
+                left: { xs: "29.3vw" },
+              }}
+            />
+          </div>
+        </Stack>
+        <Stack
+          p={5}
+          fontSize={{ xs: "20px", lg: "26px" }}
+        >
+          <div
+            className="flex flex-col !space-y-10"
+            dangerouslySetInnerHTML={{
+              __html: language === "id" ? (data?.data.descriptionIDN as string) : (data?.data.descriptionENG as string),
+            }}
           />
         </Stack>
+
+        {/*Carousel */}
+        {data && data.data.productAdvantage && (
+          <Box p={{ xs: 5, lg: 10 }}>
+            <Typography
+              gutterBottom
+              fontWeight={800}
+              sx={{ fontWeight: "bold", fontSize: { xl: "34px", md: "24px" } }}
+            >
+              More Than Just a Waterproofing
+            </Typography>
+
+            <Box
+              sx={{ overflow: "hidden", position: "relative" }}
+              ref={emblaRef}
+            >
+              <Box sx={{ display: "flex" }}>
+                {data?.data.productAdvantage.map((slide, index) => (
+                  <Box
+                    key={index}
+                    sx={{
+                      minWidth: { xl: "20%", lg: "25%", md: "30%", sm: "40%", xs: "60%" },
+                      padding: 2,
+                      borderRadius: "25px",
+                      position: "relative",
+                      height: { lg: "500px", sm: "400px", xs: "300px" },
+                      mx: 2,
+                      backgroundColor: "rgba(0,0,0,1)",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <div
+                      onClick={() =>
+                        setAdvantage({
+                          title: language === "eng" ? slide.titleENG : slide.titleIDN,
+                          description: language === "eng" ? slide.descriptionENG : slide.descriptionIDN,
+                          image: slide.heroImageUrl,
+                        })
+                      }
+                    >
+                      <Image
+                        src={slide.heroImageUrl}
+                        alt={language === "eng" ? slide.titleENG : slide.titleIDN}
+                        layout="fill"
+                        objectFit="cover"
+                        style={{ borderRadius: "25px" }}
+                        priority
+                        unoptimized
+                      />
+                      <Box
+                        sx={{
+                          position: "absolute",
+                          top: 0,
+                          width: "90%",
+                          color: "white",
+                          pt: 2,
+                          px: 1,
+                        }}
+                      >
+                        <Typography
+                          alignSelf={"start"}
+                          fontSize={{ xs: "13px", sm: "15px", md: "16px", lg: "16px", xl: "22px" }}
+                        >
+                          {language === "eng" ? slide.titleENG : slide.titleIDN}
+                        </Typography>
+                      </Box>
+                    </div>
+                  </Box>
+                ))}
+              </Box>
+            </Box>
+            {/*Carousel */}
+
+            <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
+              <IconButton
+                onClick={scrollPrev}
+                sx={{ mx: 1 }}
+              >
+                <ArrowBackIosIcon sx={{ color: "#000" }} />
+              </IconButton>
+              <IconButton
+                onClick={scrollNext}
+                sx={{ mx: 1 }}
+              >
+                <ArrowForwardIosIcon sx={{ color: "#000" }} />
+              </IconButton>
+            </Box>
+          </Box>
+        )}
+
+        <AdvantageDialog
+          description={advantage?.description}
+          img={advantage?.image}
+          open={Boolean(advantage)}
+          onClose={() => setAdvantage(undefined)}
+          title={advantage?.title}
+        />
+
+        <Box sx={{ width: { lg: "100vw" }, height: { lg: "65vh", xs: "30vh" }, position: "relative" }}>
+          <Image
+            alt="image1"
+            src={produk}
+            fill
+            style={{ objectFit: "contain" }}
+            priority
+          />
+        </Box>
+        <Stack
+          direction={"column"}
+          justifyContent={"center"}
+          width={"100vw"}
+          alignItems={"center"}
+        >
+          <Typography fontSize={{ xs: "20px", lg: "24px" }}>Tersedia Pilihan Warna:</Typography>
+          <Box
+            sx={{
+              width: { lg: "55vw", md: "60vw", xs: "80vw" },
+              height: { lg: "20vh", md: "10vh", xs: "100px" },
+              position: "relative",
+            }}
+          >
+            <Image
+              alt="image1"
+              src={colors}
+              fill
+              style={{ objectFit: "contain" }}
+              priority
+            />
+          </Box>
+        </Stack>
+        <SixthSection noBackground />
+        <Box
+          sx={{
+            width: { xs: "100vw" },
+            height: "30vh",
+            px: 2,
+            background: "linear-gradient(89.7deg, #B94BBC -18.1%, #137C3B 115.45%)",
+            backgroundSize: "cover",
+          }}
+        >
+          <Stack
+            alignItems={"center"}
+            justifyItems={"center"}
+            direction={"row"}
+            spacing={4}
+            sx={{ pt: { xs: "12vh" } }}
+          >
+            <Typography
+              variant="h2"
+              fontWeight={800}
+              color="rgba(255, 255, 255, 1)"
+              textAlign={"center"}
+              width={"65vw"}
+              sx={{ fontSize: { xl: "45px", md: "30px", xs: "15px" } }}
+            >
+              Vibrant hues endless possibilities
+            </Typography>
+            <FilledButton
+              label="View Product"
+              onClick={() => router.push("/product/multifungsi")}
+            />
+          </Stack>
+        </Box>
       </Box>
     </AppLayout>
   );
